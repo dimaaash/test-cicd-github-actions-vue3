a6b9145dd6d1f5020499bb54bf0618296f0d3316
Dmitry Kuznetsov <dkuznetsov@eqcare.com>
2020-10-18 21:29:57 -0400

    tests
    tests
    
    tests
    
    tests
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests
    
    tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
+  collectCoverage: true,
+  testResultsProcessor: "jest-sonar-reporter",
+  coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
+  moduleFileExtensions: ["js", "json", "vue"],
   transform: {
-    "^.+\\.vue$": "vue-jest"
+    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
+    "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
+    ".+\\.(css|styl|less|sass|scss)$":
+      "<rootDir>/node_modules/jest-css-modules-transform"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(eqcare-ui|@ionic-native|vue-instantsearch|instantsearch.js|capacitor-secure-storage-plugin)/)"
+  ],
+  moduleNameMapper: {
+    "@/(.*)$": "<rootDir>/src/$1"
   }
 };
